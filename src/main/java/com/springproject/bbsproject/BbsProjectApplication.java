eb114eaefdba80dbe7e5466ea6b030278b89dafc
AikiToWeb <baekjongheon7418@gmail.com>
2022-01-19 15:29:34 +0900

    Initial commit

---
+++ src/main/java/com/springproject/bbsproject/BbsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.bbsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springproject/bbsproject/BbsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.bbsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
